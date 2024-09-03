97bfdf1f0983fa4160edc428b6a4035c5bb4184d
itdoginfo <koalav42@gmail.com>
2024-09-03 11:07:59 +0300

    Update

--- convert.py
+++ convert.py
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     # Russia inside
     Path("Russia").mkdir(parents=True, exist_ok=True)
 
-    removeDomains = {'google.com', 'github.com', 'microsoft.com', 'cloudflare-dns.com'}
+    removeDomains = {'google.com', 'github.com', 'githubusercontent.com', 'microsoft.com', 'cloudflare-dns.com' }
     urllib.request.urlretrieve("https://community.antifilter.download/list/domains.lst", "antifilter-domains.lst")
     inside_lists = ['antifilter-domains.lst', rusDomainsInsideSrc]
 
